2d4db1a9e3c4c93e5ad8ffedfd864ec6a616dfb4
Graziela182 <81112319+Graziela182@users.noreply.github.com>
2022-04-08 13:31:01 -0300

    Create Ex03.dart

---
+++ Ex03.dart
@@ -0,0 +1,8 @@
+void main() {
+     double celsius, fahrenheit, calculo;
+  celsius = 37;
+  calculo = (celsius * 1.8) + 32;
+  fahrenheit = calculo;
+  
+  print("A temperatura em Fahrenheit é: $calculo");
+  }
