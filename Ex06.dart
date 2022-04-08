52d44142ff1773736a78985abaf2ccc7b82ccf3c
Graziela182 <81112319+Graziela182@users.noreply.github.com>
2022-04-08 13:32:34 -0300

    Create Ex06.dart

---
+++ Ex06.dart
@@ -0,0 +1,13 @@
+void main() {
+  
+  double base, altura, area;
+  base = 2;
+  altura = 50;
+  area = (base * altura);
+  
+    if (area >= 100){
+       print ("Terreno grande.");
+    }
+    else 
+      print ("Terreno pequeno.");
+}
