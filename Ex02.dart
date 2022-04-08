4469936995af004b74651f716c0117c26627bb60
Graziela182 <81112319+Graziela182@users.noreply.github.com>
2022-04-08 13:30:33 -0300

    Create Ex02.dart

---
+++ Ex02.dart
@@ -0,0 +1,9 @@
+void main() {
+     double v1, v2, v3, v4, soma;
+      v1 = 6;
+      v2 = 3;
+      v3 = 9;
+      v4 = 8;
+      soma = (v1 + v2 + v3 + v4) / 4;
+     print("A média aritmética é igual: $soma");
+  }
