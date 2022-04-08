f540f0cda925b84d05fc176f5295820b3a5c854a
Graziela182 <81112319+Graziela182@users.noreply.github.com>
2022-04-08 13:29:40 -0300

    Create Ex01.dart

---
+++ Ex01.dart
@@ -0,0 +1,10 @@
+void main() {
+     double base, altura, media;
+ 
+       altura = 10;
+       base = 20;
+  
+       media = (altura * base);
+  
+       print ("A área do retangulo é $media");
+  }
